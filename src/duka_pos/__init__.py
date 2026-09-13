efc661ef027515a9cdf872cea80e7c3596f6aae0
kelvinblack3-beep <kelvinblack3@icloud.com>
2026-09-13 17:43:59 +0300

    chore: initialize duka-pos repository

---
+++ src/duka_pos/__init__.py
@@ -0,0 +1,10 @@
+"""Duka POS package.
+
+Local-first point of sale for Kenyan mini-marts.
+
+Milestone 0 contains only the importable package foundation.
+No sales, inventory, payments, receipts, or integration behaviour
+is implemented in this module.
+"""
+
+__version__ = "0.0.1"
